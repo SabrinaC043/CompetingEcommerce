2b75b217ae5919f76bcb93d6d60cb600cc3c3ae9
Sabrina Carrion <SabrinaCarrion43@gmail.com>
2022-10-02 12:51:23 -0600

    Classes and categories

--- models/Product.js
+++ models/Product.js
@@ -2,14 +2,43 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowsNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    }, // define columns
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowsNull: false,
+
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowsNull: false,
+      validate: { isDecimal: true, }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      value: {
+        default: 10,
+        validate: { isNumeric: true, }
+      },
+      category_id: {
+        type: DataTypes.INTEGER,
+        // belongsTo(Category)
+      },
+    }
   },
   {
     sequelize,
